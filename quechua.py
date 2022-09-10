4e6eac2a25a02d31a5df00ae3dc11c6b6c369965
Javier Vera Zúñiga <jxvera@gmail.com>
2022-09-10 14:09:40 -0500

    Update quechua.py

--- quechua.py
+++ quechua.py
@@ -4,10 +4,6 @@ import streamlit as st
 
 numeros_1_10 = ['huk', 'iskay', 'kimsa', 'tawa', 'pichqa', 'suqta', 'qanchis', 'pusaq', 'isqun', 'chunka']
 
-
-# In[2]:
-
-
 ## para esto creamos una función que recibe N (el número decimal) y  entrega el número en palabras (Quechua)
 
 ## input:::N: número decimal
@@ -19,31 +15,6 @@ def decimal_quec(N):
     numero_quechua = numeros_1_10[posicion]
     ## entregamos el número
     return numero_quechua
-
-
-# In[3]:
-
-
-N = 10
-
-decimal_quec(N)
-
-
-# Nuestra función solo sirve para números menores o iguales 10, ¿cómo agregamos esta información? En esto, hay una condición lógica encubierta: **si** el número es menor a 10, la función tiene sentido; en otro caso, la función va a fallar. Esto nos recuerda al condicional de la forma **if A, (then) B**, en donde si se cumple cierta condición **A**, ocurrirá **B**. ¿Cómo traducimos estas ideas a **Python**? Vamos por partes:
-#
-# - Condición **if A**: el número es menor a 10
-# ```python
-# if N <= 10:
-# ```
-# Noten la coloración de **if**, el uso de **in** y el **:** final.
-#
-# - Condición **(then) B**: esta consecuencia equivale a que si se cumple **A**, entonces usamos **sufijo1**
-# ```python
-# if N <= 10:
-#     posicion = N - 1
-# ```
-
-# In[4]:
 
 
 ## input:::N: número decimal
@@ -78,7 +49,7 @@ def decimal_quec(N):
 
 ## input:::N: número decimal
 ## output::: número en palabras (Quechua)
-def decimal_quec(N):
+def decimal_quec(N,suffix):
 
     ## desarmamos el número. Interesa la unidad
     unidad = N - 10
@@ -94,12 +65,8 @@ def decimal_quec(N):
     elif N > 10 and N < 20:
         ## restamos uno porque la posición empieza en cero
         numero_quechua = 'chunka' + ' ' + numeros_1_10[unidad - 1]
-        ## si termina en vocal, ponemos -yuq
-        if numero_quechua[-1] in ['a','e','i','o','u']:
-            numero_quechua = numero_quechua + '-yuq'
-        ## si termina en consonante
-        else:
-            numero_quechua = numero_quechua + '-ni' + '-yuq'
+        ## -yuq
+        numero_quechua =
         ## entregamos el número
         return numero_quechua
     ## en otro caso
@@ -107,9 +74,26 @@ def decimal_quec(N):
         print('no conozco números tan grandes :(')
 
 
+
+## función para saber si sufijamos con -yuq
+
+def yuq(s,suffix):
+    if not (s.endswith('chunka') or s.endswith('pachak') or s.endswith('waranqa')):
+        if suffix == True:
+            if s[-1] in ['a','e','i','o','u']:
+                s = s + '-yuq'
+            else:
+                s = s + '-ni' + '-yuq'
+            return s
+        else:
+            return s
+    else:
+        return s
+
+
 ## input:::N: número decimal
 ## output::: número en palabras (Quechua)
-def decimal_quec_99(N):
+def decimal_quec_99(N,suffix):
 
     ## desarmamos el número
     if N > 10:
@@ -128,12 +112,8 @@ def decimal_quec_99(N):
     elif N > 10 and N < 20:
         ## restamos uno porque la posición empieza en cero
         numero_quechua = 'chunka' + ' ' + numeros_1_10[unidad - 1]
-        ## si termina en vocal, ponemos -yuq
-        if numero_quechua[-1] in ['a','e','i','o','u']:
-            numero_quechua = numero_quechua + '-yuq'
-        ## si termina en consonante
-        else:
-            numero_quechua = numero_quechua + '-ni' + '-yuq'
+        ## -yuq
+        numero_quechua = yuq(numero_quechua,suffix)
         ## entregamos el número
         return numero_quechua
     ## dos condiciones simultáneas
@@ -142,12 +122,8 @@ def decimal_quec_99(N):
             numero_quechua = numeros_1_10[decena-1] + ' ' + 'chunka'
         else:
             numero_quechua = numeros_1_10[decena-1] + ' ' + 'chunka' + ' ' + numeros_1_10[unidad - 1]
-            ## si termina en vocal, ponemos -yuq
-            if numero_quechua[-1] in ['a','e','i','o','u']:
-                numero_quechua = numero_quechua + '-yuq'
-            ## si termina en consonante
-            else:
-                numero_quechua = numero_quechua + '-ni' + '-yuq'
+            ## -yuq
+            numero_quechua = yuq(numero_quechua,suffix)
         return numero_quechua
     ## en otro caso
     else:
@@ -155,12 +131,12 @@ def decimal_quec_99(N):
 
 
 
-def decimal_quec_999(N):
+def decimal_quec_999(N, suffix):
     ## centena
     centena=int(str(N)[0])
     ## número menor a 100
     if N<100:
-        return decimal_quec_99(N)
+        return decimal_quec_99(N, suffix)
     ## el número es 100
     elif N==100:
         return 'pachak'
@@ -169,25 +145,23 @@ def decimal_quec_999(N):
         return numeros_1_10[centena - 1]+' '+'pachak'
     ## números intermedios
     else:
-        D = decimal_quec_99(int(str(N)[1:]))
-        if not D.endswith('chunka') and not D.endswith('-yuq'):
-            if D[-1] in ['a','e','i','o','u']:
-                D = D + '-yuq'
-            ## si termina en consonante
-            else:
-                D = D + '-ni' + '-yuq'
+        N_d = int(str(N)[1:])
+        D = decimal_quec_99(N_d, suffix)
+        if N_d < 10:
+            D = yuq(D,suffix)
         if centena==1:
             return 'pachak'+' '+D
         else:
             return numeros_1_10[centena - 1]+' '+'pachak'+' '+D
 
 
-def decimal_quec_9999(N):
+
+def decimal_quec_9999(N, suffix):
     ## mil
     mil=int(str(N)[0])
     ## número menor a 1000
     if N<1000:
-        return decimal_quec_999(N)
+        return decimal_quec_999(N, suffix)
     ## el número es 1000
     elif N==1000:
         return 'waranqa'
@@ -196,44 +170,59 @@ def decimal_quec_9999(N):
         return numeros_1_10[mil - 1]+' '+'waranqa'
     ## números intermedios
     else:
+        N_d = int(str(N)[1:])
+        D = decimal_quec_999(int(str(N)[1:]), suffix)
+        if N_d < 10:
+            D = yuq(D,suffix)
         if mil==1:
-            return 'waranqa'+' '+decimal_quec_999(int(str(N)[1:]))
+            return 'waranqa'+' '+D
         else:
-            return numeros_1_10[mil - 1]+' '+'waranqa'+' '+decimal_quec_999(int(str(N)[1:]))
+            return numeros_1_10[mil - 1]+' '+'waranqa'+' '+D
 
 
-def decimal_quec_99999(N):
+def decimal_quec_99999(N, suffix):
     ## 100mil
     decenamil=str(N/1000).split('.')
     ## número menor a 1000
     if N<10000:
-        return decimal_quec_9999(N)
+        return decimal_quec_9999(N,suffix)
     ## el número es 10000
     elif N==10000:
         return 'chunka waranqa'
     ## múltiplos de 10000
     elif N%1000==0 and N>10000 and N<100000:
-        return decimal_quec_9999(int(N/1000))+' '+'waranqa'
+        return decimal_quec_9999(int(N/1000),-1*suffix)+' '+'waranqa'
     ## números intermedios
     else:
-        return decimal_quec_9999(int(decenamil[0]))+' '+'waranqa'+' '+decimal_quec_9999(int(decenamil[1]))
+        N_d = int(str(N)[1:])
+        D = decimal_quec_9999(int(decenamil[0]),-1*suffix)+' '+'waranqa'+' '+decimal_quec_9999(int(decenamil[1]),suffix)
+        N_d = int(decenamil[1])
+        if N_d < 10:
+            D = yuq(D,True)
+        return D
 
 
-def decimal_quec_999999(N):
+def decimal_quec_999999(N, suffix):
     ## 100mil
     decenamil=str(N/1000).split('.')
     ## número menor a 1000
     if N<100000:
-        return decimal_quec_99999(N)
+        return decimal_quec_99999(N, suffix)
     ## el número es 10000
     elif N==100000:
         return 'pachak waranqa'
     ## múltiplos de 10000
     elif N%1000==0 and N>100000 and N<1000000:
-        return decimal_quec_9999(int(N/1000))+' '+'waranqa'
+        return decimal_quec_9999(int(N/1000), -1*suffix)+' '+'waranqa'
     ## números intermedios
     else:
-        return decimal_quec_9999(int(decenamil[0]))+' '+'waranqa'+' '+decimal_quec_99999(int(decenamil[1]))
+        N_d = int(str(N)[1:])
+        D = decimal_quec_9999(int(decenamil[0]), -1*suffix)+' '+'waranqa'+' '+decimal_quec_99999(int(decenamil[1]), suffix)
+        N_d = int(decenamil[1])
+        if N_d < 10:
+            D = yuq(D,True)
+        return D
+
 
 
 st.title("**Números en Quechua!**")
